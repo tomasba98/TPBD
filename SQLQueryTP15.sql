4e70ca8f3b18a0668a059d67fb4983672947e6f5
tomasba98 <tomasbava98@gmail.com>
2023-10-10 17:23:51 -0300

    anashe

--- SQLQueryTP15.sql
+++
@@ -1,8 +0,0 @@
-SELECT
-    ROW_NUMBER() OVER (ORDER BY partido_id, jugador_id) as ID,
-    partido_id,
-    jugador_id,
-    stat_tiros_triples_convertidos_id,
-    stat_tiros_triples_convertidos_valor
-FROM datos_staging
-ORDER BY partido_id, jugador_id;
